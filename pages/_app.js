b1d75c4242bfe10ec9685fe71e0aed88d9546344
Aaron Stroud <astroud@gmail.com>
2021-05-26 11:08:37 -0700

    build: ESLint, Styled Components + global CSS vars

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
-import '../styles/globals.css'
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap');
+
+  * {  // css reset
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+    }
+
+  html {
+    --color-accent: #D87D4A;
+    --background-footer: #101010;
+    --background-light: #F1F1F1;
+    --background: #FAFAFA;
+    
+    --color-accent-hover: #fbaf85;
+    --color-white: #FFFFFF;  // Needs a semantic or more descriptive name
+
+    --background-navbar: #000000;
+    --background-hero: #000000;
+  }
+
+  body {
+    font-family: 'Manrope', sans-serif;
+  }
+`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
